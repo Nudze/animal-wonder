bdd4f880dbfdc3912ee85b4c5e80a644e9ca4c84
nudzejma <nudzekalo@gmail.com>
2020-05-30 14:29:31 +0200

    add error flash messages to middleware functions

--- index.js
+++ index.js
@@ -44,6 +44,8 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
 	res.locals.user = req.user;
+	res.locals.error = req.flash("error");
+	res.locals.success = req.flash("success");
 	next();
 });
 
--- middleware/index.js
+++ middleware/index.js
@@ -7,6 +7,7 @@ middlewareObj.isLoggedIn = (req, res, next) => {
 	if (req.isAuthenticated()) {
 		return next();
 	}
+	req.flash("error", "You need to be logged in to do that!");
 	res.redirect("/login");
 };
 
@@ -14,16 +15,19 @@ middlewareObj.commentAuthorization = (req, res, next) => {
 	if (req.isAuthenticated()) {
 		Comment.findById(req.params.commentID, (err, comment) => {
 			if (err) {
+				req.flash("error", "Comment not found!");
 				res.redirect("back");
 			} else {
 				if (comment.author.id.equals(req.user._id)) {
 					next();
 				} else {
+					req.flash("error", "You don't have permission to do that!");
 					res.redirect("back");
 				}
 			}
 		});
 	} else {
+		req.flash("error", "You need to be logged in to do that!");
 		res.redirect("/login");
 	}
 };
@@ -32,16 +36,19 @@ middlewareObj.animalAuthorization = (req, res, next) => {
 	if (req.isAuthenticated()) {
 		Animal.findById(req.params.id, (err, animal) => {
 			if (err) {
+				req.flash("error", "Animal not found!");
 				res.redirect("back");
 			} else {
 				if (animal.author.id.equals(req.user._id)) {
 					next();
 				} else {
+					req.flash("error", "You don't have permission to do that!");
 					res.redirect("back");
 				}
 			}
 		});
 	} else {
+		req.flash("error", "You need to be logged in to do that!");
 		res.redirect("/login");
 	}
 };
